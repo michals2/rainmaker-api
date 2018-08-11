cfa428230b0167df607d828f0d8976bf8b55a1a0
Luke <luke.michals@gmail.com>
2018-08-11 13:46:13 -0700

    add request for getting all company metadata

--- src/api/index.js
+++ src/api/index.js
@@ -5,19 +5,36 @@ const schema = buildSchema(`
   type Query {
     hello: String
     hi: String
+    company: Company
+    allCompaniesMetaData: [CompanyMetaData]
+  }
+  type Company {
     stockPrices: [Float]
+    name: String
+    symbol: String
+  }
+  type CompanyMetaData {
+    symbol: String
+    name: String
   }
 `);
 
 const resolvers = {
   hello: () => "Hello world",
   hi: () => "Hi world",
-  stockPrices: async () => {
+  company: async () => {
     return await axios
       .get("https://api.iextrading.com/1.0/stock/aapl/chart/1d")
       .then(({ data }) => {
         const values = data.map(d => d.average);
-        return values;
+        return { stockPrices: values };
+      });
+  },
+  allCompaniesMetaData: async () => {
+    return await axios
+      .get("https://api.iextrading.com/1.0/ref-data/symbols")
+      .then(({ data }) => {
+        return data.map(d => ({ symbol: d.symbol, name: d.name }));
       });
   }
 };
