b7cd24bdb5be70889576e22c515392a49090a704
Luke <luke.michals@gmail.com>
2018-08-11 12:54:43 -0700

    resolve async call to IEX API

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,25 @@
 const { buildSchema } = require("graphql");
+const axios = require("axios");
 
 const schema = buildSchema(`
   type Query {
     hello: String
+    hi: String
+    stockPrices: [Float]
   }
 `);
 
 const resolvers = {
-  hello: () => "Hello world"
+  hello: () => "Hello world",
+  hi: () => "Hi world",
+  stockPrices: async () => {
+    return await axios
+      .get("https://api.iextrading.com/1.0/stock/aapl/chart/1d")
+      .then(({ data }) => {
+        const values = data.map(d => d.average);
+        return values;
+      });
+  }
 };
 
 module.exports = {
