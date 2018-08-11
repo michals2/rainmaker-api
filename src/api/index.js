1f984b6f3cc3217f8b9221770a65ad05e924526e
Luke <luke.michals@gmail.com>
2018-08-11 16:52:34 -0700

    move async logic to context

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,10 @@
-const { buildSchema } = require("graphql");
 const axios = require("axios");
 
 const typeDefs = `
   type Query {
     company(symbol: String!): Company
     companies(limit: Int): [Company]
+    companyStockPrice(symbol: String!): [Float]
   }
   type Company {
     name: String
@@ -14,31 +14,72 @@ const typeDefs = `
   }
 `;
 
+const context = req => ({
+  ...req,
+  externalApiRequestHelpers: {
+    getCompaniesMetaData: limit =>
+      axios.get("https://api.iextrading.com/1.0/ref-data/symbols"),
+    getCompanyStockPriceHistory: (symbol, duration = "1d") =>
+      axios.get(
+        `https://api.iextrading.com/1.0/stock/${symbol}/chart/${duration}`
+      )
+  }
+});
+
 const resolvers = {
   Query: {
-    company: async (_, { symbol }) => {
-      return await axios
+    company: (_, { symbol }, ctx) => {
+      return axios
         .get(`https://api.iextrading.com/1.0/stock/${symbol}/chart/1d`)
         .then(({ data }) => {
           const values = data.map(d => d.average);
           return { historicalStockPrices: values };
         });
     },
-    companies: async (_, { limit }) => {
-      return await axios
-        .get("https://api.iextrading.com/1.0/ref-data/symbols")
-        .then(({ data }) => {
-          return data.slice(0, limit).map(d => ({
-            name: d.name,
-            symbol: d.symbol,
-            logo: d.logo
-          }));
+    companies: async (_, { limit = 2 }, ctx, info) => {
+      const fields = info.fieldNodes[0].selectionSet.selections.map(
+        s => s.name.value
+      );
+
+      const {
+        getCompaniesMetaData,
+        getCompanyStockPriceHistory
+      } = ctx.externalApiRequestHelpers;
+      const companiesMetaData = await getCompaniesMetaData(
+        limit
+      ).then(({ data }) => {
+        return data.slice(0, limit).map(d => ({
+          name: d.name,
+          symbol: d.symbol,
+          logo: d.logo
+        }));
+      });
+
+      if (fields.some(f => f === "historicalStockPrices")) {
+        const promiseArr = companiesMetaData.map(m => {
+          return getCompanyStockPriceHistory(m.symbol);
         });
+        const companiesStockPriceHistory = await Promise.all(
+          promiseArr
+        ).then(responseArr => {
+          return responseArr.map(res =>
+            res.data.map(dataPoint => dataPoint.average)
+          );
+        });
+        const mergedData = companiesMetaData.map((companyMetaData, i) => ({
+          ...companyMetaData,
+          historicalStockPrices: companiesStockPriceHistory[i]
+        }));
+        return mergedData;
+      } else {
+        return companiesMetaData;
+      }
     }
   }
 };
 
 module.exports = {
   typeDefs,
-  resolvers
+  resolvers,
+  context
 };
--- src/server.js
+++ src/server.js
@@ -1,21 +1,6 @@
-// const { buildSchema } = require("graphql");
-// const server = require("express-graphql");
-// const CORS = require("micro-cors")();
 const { GraphQLServer } = require("graphql-yoga");
 
-const { typeDefs, resolvers } = require("./api");
+const { typeDefs, resolvers, context } = require("./api");
 
-// const typeDefs = `
-//   type Query {
-//     hello(name: String): String!
-//   }
-// `;
-
-// const resolvers = {
-//   Query: {
-//     hello: (_, { name }) => `Hello ${name || "World"}`
-//   }
-// };
-
-const server = new GraphQLServer({ typeDefs, resolvers });
+const server = new GraphQLServer({ typeDefs, resolvers, context });
 server.start(() => console.log("Server is running on localhost:4000"));
